b24d5d56bf846fde6653450cb32e53d95e20ea47
ehJoao <joao_mmn@hotmail.com>
2023-03-30 10:07:02 +0100

    Exercicio 6

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+
+
+//
+// 1.Implemente uma função que determine se um número (passado por argumento) é par ou ímpar.
+
+
+}
